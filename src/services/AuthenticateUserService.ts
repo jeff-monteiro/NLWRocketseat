bd07b539725b28b3fff92615bfddc555ff755059
Jefferson <jeffmonteiro61@icloud.com>
2021-10-20 23:26:08 -0300

    routes

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,9 +1,15 @@
-
+/**
+* Receber code(string)
+* Recuperar o acess_token no github
+* Verificar se o usuário existe no DB
+*----SIM = Gera um token
+*----NAO = Cria no DB, gera um token
+ */
 
 class AuthenticateUserService{
   async execute(code: string){
-    
+
   }
 }
 
-export { AuthenticateUserService }
+export { AuthenticateUserService };
